a3ad60a061c73be82ba1e04e4ab24452bb721e2a
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-11-07 14:39:05 +0000

    feat: Less zombie processes after loading data

--- convert_multiple_vcfs.sh
+++ convert_multiple_vcfs.sh
@@ -2,7 +2,27 @@ VCF_DIR=files/vcf/files_to_read
 SCRIPT_PATH=genomicVariations_vcf.py
 OUTPUTS_DIR=outputs_cmds
 
-for chr in $(seq 1 22) X
-do
-    nohup python3 $SCRIPT_PATH $VCF_DIR/*_chr${chr}.vcf.gz &>> $OUTPUTS_DIR/$chr &
-done
+# Run it with `nohup bash convert_multiple_vcfs.sh &`
+
+function main_func {
+	pids=""
+
+	# launch processes
+	for chr in $(seq 1 22) X
+	do
+		nohup python3 $SCRIPT_PATH $VCF_DIR/*_chr${chr}.vcf.gz &>> $OUTPUTS_DIR/$chr &
+		pids="$pids $!"
+	done
+
+	# wait for processes to finish
+	c=0
+	for p in $pids
+	do
+		c=$(expr $c + 1)
+		echo "waiting $c process, with PID $p"
+		wait $p
+	done
+}
+
+# run it
+main_func
